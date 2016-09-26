dc0ece0d371b362d32e249106214e4bd7d47a8bf
unknown <akk000@SHI58495GU2-E74.kih.kmart.com>
2016-09-26 20:16:47 +0530

    Campaign save json modification

--- dashboard/js/campaign.js
+++ dashboard/js/campaign.js
@@ -5,9 +5,12 @@ var Campaign = {
 		this.notNull = Campaign.notNull;
 		this.campaignTable = Campaign.campaignTable;
 		this.campaignData = Campaign.campaignData;
+		this.ajaxMethod = Campaign.ajaxMethod;
 		this.tableOptions = function(columns){
+			this.dom = "lftip";
 			this.paging = true;
 			this.serverSide = true;
+			this.cache = true
 			this.processing = true;
 			this.destroy = true;
 			this.ajax = {};
@@ -19,30 +22,44 @@ var Campaign = {
 	init : function(){
 		var that = this;
 		$("#creteCampaign").on("click",function(){
+			that.ajaxMethod = "POST";
+			$("input:text").val("");
 			$("#campaignModal").modal('show');
 		});
 		
 		var columns = [
-				{ data : 'name'
+				{ data : 'campaign_details.name'
 				},
-				{ data : 'money'
+				{ data : 'campaign_details.money'
 				},
-				{ data : 'category'
+				{ data : 'campaign_details.category'
 				},
-				{ data : 'conversion_ratio' 
+				{ data : 'campaign_details.conversion_ratio' 
 				},
-				{ data : 'period'
+				{ data : 'campaign_details.period'
 				},
 				{ data : null,
 				  render : function(data){
-					return "<button class='btn btn-table' onclick='GlobalVar.Campaign.Edit("+data.id+")'> <i class='icon-edit icon-size'> </i> </button> <button class='btn btn-table'> <i class='icon-trash icon-size'> </i> </button>"
+					return "<button class='btn btn-table' onclick='GlobalVar.Campaign.Edit("+data.campaign_details.campaign_id+")'> <i class='icon-edit icon-size'> </i> </button> <button class='btn btn-table'> <i class='icon-trash icon-size'> </i> </button>"
 				  }
 				}
 			];
 		
 		var tableOptions = new this.tableOptions(columns);
-		tableOptions.ajax.url = exposedAPIs.baseUrl + "/api/campaign/";
-		tableOptions.ajax.dataSrc = "data";
+		tableOptions.ajax = function(data,callback,settings){
+			data ="";
+			$.ajax({
+				headers: {"authorization":exposedAPIs.authToken},
+				url: exposedAPIs.baseUrl +"/api/campaign/",
+				success:function(res){
+					callback({
+						recordsTotal:res.data.length,
+						data:res.data
+					})
+				}
+				
+			})
+		}
 		that.campaignTable = $("#campaignTable").DataTable(tableOptions);
 		
 	/* next- previous code */
@@ -55,7 +72,7 @@ var Campaign = {
 		wrapper.append('<div class="clear clearfix"> </div><div class="modal-footer"><button type="button" class="btn btn-cus" id="previous"><i class="icon-chevron-sign-left"></i></button> <button type="button" class="btn btn-cus" id="next" style="margin-bottom: 5px">  <i class="icon-chevron-sign-right"></i></button> <button type="button" id="saveCampaign" class="btn btn-greyBlue" style="margin-bottom: 5px"> <i class="icon-thumbs-up"></i> Confirm </button></div>');
 
 		$('#previous').hide();
-		$('#addRule').hide();
+		$('#saveCampaign').hide();
 
 		var liElements = content.children();
 		liElements.hide();
@@ -92,16 +109,18 @@ var Campaign = {
 			$('#next').click(function () {
 				that.campaignData = {};
 				that.campaignData.offer_details = {}
+				if(that.notNull($("#offerid").val()) && that.ajaxMethod == "PUT");
+					that.campaignData.offer_details.offer_id = $("#offerid").val();
 				if(that.notNull($("#OfferType").val()))
-					that.campaignData.offer_details.offertype = $("#OfferType").val();
+					that.campaignData.offer_details.offer_type = $("#OfferType").val();
 				if(that.notNull($("#mindollar").val()))
-					that.campaignData.offer_details.min = $("#mindollar").val();
+					that.campaignData.offer_details.min_value = $("#mindollar").val();
 				if(that.notNull($("#maxdollar").val()))
-					that.campaignData.offer_details.max = $("#maxdollar").val();
+					that.campaignData.offer_details.max_value = $("#maxdollar").val();
 				if(that.notNull($("#validity").val()))
-					that.campaignData.offer_details.validity = $("#validity").val();
-				if(that.notNull($("#memberIssurance").val()))
-					that.campaignData.offer_details.memberIssurance = $("#memberIssurance").val();
+					that.campaignData.offer_details.valid_till = $("#validity").val();
+				if(that.notNull($("#memberissuance").val()))
+					that.campaignData.offer_details.member_issuance = $("#memberissuance").val();
 				counter++;
 				swapContent();
 			});
@@ -109,11 +128,14 @@ var Campaign = {
 			$('#previous').click(function () {
 				counter--;
 				swapContent();
+				$('#saveCampaign').hide();
 			});
 		}
 		$("#saveCampaign").on("click",function(event){
 			event.preventDefault();
 			that.campaignData.campaign_details= {};
+			if(that.notNull($("#campaignid").val()) && that.ajaxMethod == "PUT");
+				that.campaignData.campaign_details.campaign_id = $("#campaignid").val();
 			if(that.notNull($("#campaignName").val()))
 				that.campaignData.campaign_details.name = $("#campaignName").val();
 			if(that.notNull($("#money").val()))
@@ -127,13 +149,13 @@ var Campaign = {
 			$.ajax({
 				headers: {"authorization":exposedAPIs.authToken},
 				url : exposedAPIs.baseUrl + "/api/campaign/",
-				type : "POST",
+				type : that.ajaxMethod,
 				contentType: "application/json",
 				data: JSON.stringify(that.campaignData),
 				success: function(data){
 					$("#campaignModal").modal('hide');
 					that.campaignTable.draw();
-					alert(data);
+					alert(data.message);
 				},
 				error: function(error){
 					$("#campaignModal").modal('hide');
@@ -158,17 +180,28 @@ var Campaign = {
 	
 	Edit : function(id){
 		var that = this;
+		that.ajaxMethod = "PUT";
 		$.ajax({
 			headers:{"authorization":exposedAPIs.authToken},
 			type:"GET",
-			url: exposedAPIs.baseUrl+"/api/campaign/"+id,
-				success:function(data){
-					$("#campaignId").val(data.data.id);
-					$("#campaignName").val(data.data.name);
-					$("#money").val(data.data.money);
-					$("#category").val(data.data.category);
-					$("#conversionratio").val(data.data.conversion_ratio);
-					$("#period").val(data.data.period);
+			url: exposedAPIs.baseUrl+"/api/campaign/"+id+"/",
+				success:function(res){
+					var data = res.data[0];
+					$("#offerid").val(data.offer_details.offer_id);
+					$("#OfferType").val(data.offer_details.offer_type);
+					$("#mindollar").val(data.offer_details.min_value);
+					$("#maxdollar").val(data.offer_details.max_value);
+					var validity = data.offer_details.valid_till.split('-');
+					validity = validity.reverse();
+					validity = validity.join("-");
+					document.getElementById("validity").value = validity;
+					$("#memberissuance").val(data.offer_details.member_issuance);
+					$("#campaignid").val(data.campaign_details.campaign_id);
+					$("#campaignName").val(data.campaign_details.name);
+					$("#money").val(data.campaign_details.money);
+					$("#category").val(data.campaign_details.category);
+					$("#conversionratio").val(data.campaign_details.conversion_ratio);
+					$("#period").val(data.campaign_details.period);
 					
 					$("#campaignModal").modal("show");
 				},
