59784bfc66bc5bd67c3c3776a5b059830c772c7b
unknown <akk000@SHI58495GU2-E74.kih.kmart.com>
2016-09-26 13:48:14 +0530

    Campaign add,edit and fetch

---
+++ dashboard/js/Globalvar.js
@@ -0,0 +1,4 @@
+var exposedAPIs = {
+	"baseUrl": "http://172.16.1.105:8000",
+	"authToken": "Token b760b52fc2862f37fea29c43caf2fd56b93679fd"
+}
--- dashboard/js/campaign.js
+++ dashboard/js/campaign.js
@@ -1,8 +1,15 @@
 var Campaign = {
 	Constructor : function(){
 		this.init = Campaign.init;
+		this.Edit = Campaign.Edit;
+		this.notNull = Campaign.notNull;
+		this.campaignTable = Campaign.campaignTable;
+		this.campaignData = Campaign.campaignData;
 		this.tableOptions = function(columns){
 			this.paging = true;
+			this.serverSide = true;
+			this.processing = true;
+			this.destroy = true;
 			this.ajax = {};
 			this.ajax.dataSrc = "";
 			this.columns = columns;
@@ -10,60 +17,13 @@ var Campaign = {
 		
 	},
 	init : function(){
+		var that = this;
 		$("#creteCampaign").on("click",function(){
 			$("#campaignModal").modal('show');
 		});
 		
-		var data = [
-			{
-				"comp_name" : "First Compaign",
-				"money": "$100",
-				"category": "Approval",
-				"conversion_ratio": "6",
-				"period": "2 weeks",
-			},
-			{
-				"comp_name" : "Second Compaign",
-				"money":       "$100",
-				"category":   "Not Approval",
-				"conversion_ratio": "10",
-				"period": "1 weeks",
-			},
-			{
-				"comp_name" : "Third Compaign",
-				"money": "$100",
-				"category": "Approval",
-				"conversion_ratio": "6",
-				"period": "2 weeks",
-			},
-			{
-				"comp_name" : "Fourth Compaign",
-				"money":       "$100",
-				"category":   "Not Approval",
-				"conversion_ratio": "10",
-				"period": "1 weeks",
-			},
-			{
-				"comp_name" : "Fifth Compaign",
-				"money": "$100",
-				"category": "Approval",
-				"conversion_ratio": "6",
-				"period": "2 weeks",
-			},
-			{
-				"comp_name" : "Sixth Compaign",
-				"money":       "$100",
-				"category":   "Not Approval",
-				"conversion_ratio": "10",
-				"period": "1 weeks",
-			}
-		];  
-
-
-		$("#campaignTable").DataTable({
-			data : data,
-			columns: [
-				{ data : 'comp_name'
+		var columns = [
+				{ data : 'name'
 				},
 				{ data : 'money'
 				},
@@ -74,12 +34,16 @@ var Campaign = {
 				{ data : 'period'
 				},
 				{ data : null,
-				  render : function(){
-					return "<button class='btn btn-table'> <i class='icon-edit icon-size'> </i> </button> <button class='btn btn-table'> <i class='icon-trash icon-size'> </i> </button>"
+				  render : function(data){
+					return "<button class='btn btn-table' onclick='GlobalVar.Campaign.Edit("+data.id+")'> <i class='icon-edit icon-size'> </i> </button> <button class='btn btn-table'> <i class='icon-trash icon-size'> </i> </button>"
 				  }
 				}
-			]
-	    }); 
+			];
+		
+		var tableOptions = new this.tableOptions(columns);
+		tableOptions.ajax.url = exposedAPIs.baseUrl + "/api/campaign/";
+		tableOptions.ajax.dataSrc = "data";
+		that.campaignTable = $("#campaignTable").DataTable(tableOptions);
 		
 	/* next- previous code */
 		// next previous code
@@ -88,7 +52,7 @@ var Campaign = {
 		content.wrap('<div id="wrapper"></div>');
 
 		var wrapper = $('#wrapper');
-		wrapper.append('<div class="clear clearfix"> </div><div class="modal-footer"><button type="button" class="btn btn-cus" id="previous"><i class="icon-chevron-sign-left"></i></button> <button type="button" class="btn btn-cus" id="next" style="margin-bottom: 5px">  <i class="icon-chevron-sign-right"></i></button> <button type="button" id="addRule" class="btn btn-greyBlue" style="margin-bottom: 5px"> <i class="icon-thumbs-up"></i> Confirm </button></div>');
+		wrapper.append('<div class="clear clearfix"> </div><div class="modal-footer"><button type="button" class="btn btn-cus" id="previous"><i class="icon-chevron-sign-left"></i></button> <button type="button" class="btn btn-cus" id="next" style="margin-bottom: 5px">  <i class="icon-chevron-sign-right"></i></button> <button type="button" id="saveCampaign" class="btn btn-greyBlue" style="margin-bottom: 5px"> <i class="icon-thumbs-up"></i> Confirm </button></div>');
 
 		$('#previous').hide();
 		$('#addRule').hide();
@@ -107,12 +71,12 @@ var Campaign = {
 					$('#previous').hide();
 				} else {
 					$('#previous').show();
-					$('#addRule').hide();
+					$('#saveCampaign').hide();
 				}
 
 				if (counter == liElementCount - 1) {
 					$('#next').hide();
-					$('#addRule').show();
+					$('#saveCampaign').show();
 				} else {
 					$('#next').show();
 				}
@@ -122,10 +86,22 @@ var Campaign = {
 			}
 
 			if(liElements.last() == true){
-				$('#addRule').show();
+				$('#saveCampaign').show();
 			}
 			
 			$('#next').click(function () {
+				that.campaignData = {};
+				that.campaignData.offer_details = {}
+				if(that.notNull($("#OfferType").val()))
+					that.campaignData.offer_details.offertype = $("#OfferType").val();
+				if(that.notNull($("#mindollar").val()))
+					that.campaignData.offer_details.min = $("#mindollar").val();
+				if(that.notNull($("#maxdollar").val()))
+					that.campaignData.offer_details.max = $("#maxdollar").val();
+				if(that.notNull($("#validity").val()))
+					that.campaignData.offer_details.validity = $("#validity").val();
+				if(that.notNull($("#memberIssurance").val()))
+					that.campaignData.offer_details.memberIssurance = $("#memberIssurance").val();
 				counter++;
 				swapContent();
 			});
@@ -135,5 +111,70 @@ var Campaign = {
 				swapContent();
 			});
 		}
+		$("#saveCampaign").on("click",function(event){
+			event.preventDefault();
+			that.campaignData.campaign_details= {};
+			if(that.notNull($("#campaignName").val()))
+				that.campaignData.campaign_details.name = $("#campaignName").val();
+			if(that.notNull($("#money").val()))
+				that.campaignData.campaign_details.money = $("#money").val();
+			if(that.notNull($("#category").val()))
+				that.campaignData.campaign_details.category = $("#category").val();
+			if(that.notNull($("#conversionratio").val()))
+				that.campaignData.campaign_details.conversion_ratio = $("#conversionratio").val();
+			if(that.notNull($("#period").val()))
+				that.campaignData.campaign_details.period = $("#period").val();
+			$.ajax({
+				headers: {"authorization":exposedAPIs.authToken},
+				url : exposedAPIs.baseUrl + "/api/campaign/",
+				type : "POST",
+				contentType: "application/json",
+				data: JSON.stringify(that.campaignData),
+				success: function(data){
+					$("#campaignModal").modal('hide');
+					that.campaignTable.draw();
+					alert(data);
+				},
+				error: function(error){
+					$("#campaignModal").modal('hide');
+					that.campaignTable.draw();
+					alert("Something went wrong please try again later. "+error.statusText);
+				}
+			})
+			
+		})
+	},
+	
+	notNull: function(value){
+		if (typeof value != 'undefined' && value)
+		 {
+			return true;
+		 }
+		 else
+		 {
+			return false;
+		 }
+	},
+	
+	Edit : function(id){
+		var that = this;
+		$.ajax({
+			headers:{"authorization":exposedAPIs.authToken},
+			type:"GET",
+			url: exposedAPIs.baseUrl+"/api/campaign/"+id,
+				success:function(data){
+					$("#campaignId").val(data.data.id);
+					$("#campaignName").val(data.data.name);
+					$("#money").val(data.data.money);
+					$("#category").val(data.data.category);
+					$("#conversionratio").val(data.data.conversion_ratio);
+					$("#period").val(data.data.period);
+					
+					$("#campaignModal").modal("show");
+				},
+				error: function(error){
+					alert("Error"+error.statusText);
+				}
+		})
 	}
 }
