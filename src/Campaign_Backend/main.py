785fd6efb808966767355deb2a69b95df843117a
rampurearun <arun.r@searshc.com>
2016-10-11 12:53:33 +0530

    added generate token method from avibhor

--- src/Campaign_Backend/main.py
+++ src/Campaign_Backend/main.py
@@ -8,6 +8,7 @@ from getOfferXml import get_xml
 import httplib
 import sendEmail
 import xml.etree.ElementTree as ET
+from google.appengine.api import memcache
 
 
 DEFAULT_CAMPAIGN_NAME = 'default_campaign'
@@ -18,12 +19,23 @@ def get_campaign_key(campaign_name=DEFAULT_CAMPAIGN_NAME):
     return ndb.Key('CampaignData', campaign_name)
 
 
-class IndexPageHandler(webapp2.RequestHandler):
+class BaseHandler(webapp2.RequestHandler):
+    def handle_exception(self, exception, debug):
+        self.response.write('An error occurred.')
+        logging.exception(self, exception, debug)
+
+        if isinstance(exception, webapp2.HTTPException):
+            self.response.set_status(exception.code)
+        else:
+            self.response.set_status(500)
+
+
+class IndexPageHandler(BaseHandler):
     def get(self):
         self.response.write("CAMPAIGN-BACKEND-SERVICE")
 
 
-class SaveCampaignHandler(webapp2.RequestHandler):
+class SaveCampaignHandler(BaseHandler):
     def get(self):
         offer_data = self.request.get('offer_data')
         logging.info('****offerdata: %s', offer_data)
@@ -42,7 +54,7 @@ class SaveCampaignHandler(webapp2.RequestHandler):
         self.response.headers['Access-Control-Allow-Origin'] = '*'
 
 
-class GetAllCampaignsHandler(webapp2.RequestHandler):
+class GetAllCampaignsHandler(BaseHandler):
     def get(self):
         query = CampaignData.query()
         entity_list = query.fetch(100)
@@ -128,7 +140,7 @@ def save_campaign(json_data, created_time):
     logging.info('offer created with key:: %s', offer_key)
 
 
-class GetAllMembersHandler(webapp2.RequestHandler):
+class GetAllMembersHandler(BaseHandler):
     def get(self):
         query = MemberData.query()
         member_list = query.fetch(10)
@@ -140,7 +152,7 @@ class GetAllMembersHandler(webapp2.RequestHandler):
         self.response.write(json.dumps({'data': result}))
 
 
-class ActivateOfferHandler(webapp2.RequestHandler):
+class ActivateOfferHandler(BaseHandler):
     def get(self):
         offer_id = self.request.get('offer_id')
         member_id = self.request.get('member_id')
@@ -187,11 +199,19 @@ class ActivateOfferHandler(webapp2.RequestHandler):
     @classmethod
     def create_offer(cls, data):
         logging.info('**** data: %s', data)
-        url = 'esbqa-1080.searshc.com'  
-        access_token = ACCESS_TOKEN
-        # headers = {'client_id': 'OFFER_REC_SYS_QA', 'access_token': access_token,
-        # 'content-type': 'application/xml'}
+        url = 'esbqa-1080.searshc.com'
 
+        # Fetch access token from memcache
+        access_token = memcache.get(key="ACCESS_TOKEN")
+        if access_token is None:
+            logging.info('Got None access_token from memcache. Generating new token')
+            # Generating new Access Token
+            generated_access_token = ActivateOfferHandler.generateAccessToken()
+            # Writing to memcache
+            memcache.add(key="ACCESS_TOKEN", value=generated_access_token, time=10800)
+            access_token = generated_access_token
+
+        logging.info('Exisiting access_token from memcache: %s', access_token)
         logging.info('****url: %s', url)
 
         webservice = httplib.HTTPS(url)
@@ -210,10 +230,63 @@ class ActivateOfferHandler(webapp2.RequestHandler):
         logging.info('Response header: %s', header)
         logging.info('Response result: %s', result)
 
+        if status_code == 500:
+            logging.info('Request failed with status_code = 500')
+            logging.info('Generating new Access Token and retrying')
+            # Generating new Access Token
+            generated_access_token = ActivateOfferHandler.generateAccessToken()
+            # Deleting exisitng access token and Writing new token to memcache
+            memcache.delete(key="ACCESS_TOKEN")
+            logging.info('Deleted exisiting memcache ACCESS_TOKEN value')
+            memcache.add(key="ACCESS_TOKEN", value=generated_access_token, time=0)
+            logging.info('Written new ACCESS_TOKEN value to memcache')
+
+            # retry with new access token
+            webservice2 = httplib.HTTPS(url)
+            webservice2.putrequest("POST", "/rest/tellurideAS/Offer")
+            webservice2.putheader("client_id", "OFFER_REC_SYS_QA")
+            webservice2.putheader("access_token", generated_access_token)
+            webservice2.putheader("Content-type", "application/xml")
+            webservice2.endheaders()
+            webservice2.send(data)
+
+            # get the response after retry
+            status_code2, status_message2, header2 = webservice2.getreply()
+            result2 = webservice2.getfile().read()
+            logging.info('Response status_code: %s', status_code2)
+            logging.info('Response status_message: %s', status_message2)
+            logging.info('Response header: %s', header2)
+            logging.info('Response result: %s', result2)
+
         return result
 
+    @classmethod
+    def generateAccessToken(cls):
+        access_token_url = 'test-access-token-dot-syw-offers.appspot.com'
+        webservice = httplib.HTTPS(access_token_url)
+        webservice.putrequest("GET", "/generateAccessToken")
+        status_code, status_message, header = webservice.getreply()
+        access_token_result = webservice.getfile().read()
 
-class EmailOfferMembersHandler(webapp2.RequestHandler):
+        logging.info('Response access_token_result: %s', access_token_result)
+
+        try:
+            json_response = json.loads(access_token_result)
+        except ValueError:
+            # decoding failed. Retrying.
+            logging.info('Response decoding to JSON failed. Retrying to generate token.')
+            status_code, status_message, header = webservice.getreply()
+            access_token_result = webservice.getfile().read()
+            logging.info('Response access_token_result from retry : %s', access_token_result)
+            json_response = json.loads(access_token_result)
+
+        new_generated_access_token = json_response['access_token']
+        logging.info('New access_token: %s', new_generated_access_token)
+
+        return new_generated_access_token
+
+
+class EmailOfferMembersHandler(BaseHandler):
     def get(self):
         campaign_id = self.request.get('campaign_id')
 
@@ -221,15 +294,6 @@ class EmailOfferMembersHandler(webapp2.RequestHandler):
         self.response.out.write(json.dumps(obj))
 
 
-class BaseHandler(webapp2.RequestHandler):
-    def handle_exception(self, exception, debug):
-        self.response.write('An error occurred.')
-        logging.exception(self, exception, debug)
-
-        if isinstance(exception, webapp2.HTTPException):
-            self.response.set_status(exception.code)
-        else:
-            self.response.set_status(500)
 
 # [START app]
 app = webapp2.WSGIApplication([
